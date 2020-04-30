614a7958facc825e50c84b9a65bd85c44bffcca2
Joanne Yang <yang.xy9@gmail.com>
2020-04-29 20:33:09 -0400

    Updated print_board(array) -- removed hard-coding Changed marker_index variable to pos Changed arr to val Changed player to marker

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,25 +1,50 @@
-player1 = "X"
-player2 = "O"
+marker_1 = "X"
+marker_2 = "O"
 
 
 # Prints TicTacToe board with lines
-def print_board(arr):
-    board_rows, board_cols = (5, 5)
+def print_board(array):
     print()
-    for r in range(board_rows):
+    for r in range(len(array)):
+        if r == 0:
+            for c in range(len(array[r])):
+                if c == 0:
+                    print(array[r][c], end=" ")
+                else:
+                    print("| " + array[r][c], end=" ")
+        else:
+            for c in range(len(array[r]) * 2 - 1):
+                print("-", end=" ")
+            print()
+            for c in range(len(array[r])):
+                if c == 0:
+                    print(array[r][c], end=" ")
+                else:
+                    print("| " + array[r][c], end=" ")
+        print()
+    print()
+
+
+# r % 2
+# array[int(r/2)]
+def print_board2(array):
+    print()
+    for r in range(len(array) * 2 - 1):
         if r % 2 == 0:
-            i = 0
-            for c in range(board_cols):
-                if c % 2 == 0:
-                    print(arr[int(r/2)][i], end=" ")
-                    i += 1
-                elif c % 2 == 1:
-                    print("|", end=" ")
+            for c in range(len(array[int(r/2)])):
+                if c == 0:
+                    print(array[int(r/2)][c], end=" ")
+                else:
+                    print("| " + array[int(r/2)][c], end=" ")
         elif r % 2 == 1:
-            for c in range(board_cols):
+            for c in range(len(array[int(r/2)]) * 2 - 1):
                 print("-", end=" ")
         print()
     print()
+
+
+# Updates array with players' placement of marker
+# def place_marker(pos):
 
 
 # TicTacToe simulation
@@ -31,20 +56,28 @@ def ttt_simulation():
 
     while game == "Y":
         # Creates a new 2D array for TicTacToe values
-        arr_rows, arr_cols = (3, 3)
-        arr = []
+        val_rows, val_cols = (3, 3)
+        val = []
         i = 1
-        for r in range(arr_rows):
+        for r in range(val_rows):
             col = []
-            for c in range(arr_cols):
+            for c in range(val_cols):
                 col.append(str(i))
                 i += 1
-            arr.append(col)
+            val.append(col)
 
-        print_board(arr)
-        print("Player 1 is %s" % player1)
-        print("Player 2 is %s" % player2)
+        print_board(val)
+        print("Player 1 is %s" % marker_1)
+        print("Player 2 is %s" % marker_2)
         print()
+
+        # pos_1 = input("Player 1, please choose a number on the board to place your marker: ")
+        # place_marker(pos_1)
+        # print_board(val)
+
+        # pos_2 = input("Player 2, please choose a number on the board to place your marker: ")
+        # place_marker(pos_2)
+        # print_board(val)
 
         game = "N"
 
@@ -58,3 +91,4 @@ if __name__ == "__main__":
 
 # Questions
 # Allow players to select their own markers?
+# Need to add error messages to marker inputs
